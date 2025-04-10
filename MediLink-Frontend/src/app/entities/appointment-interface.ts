5794686c07cde13ce08594369c21663930359b7f
Angel GMC <angel777pro12345@gmail.com>
2025-04-10 10:30:36 -0600

    Updates in Frontend

--- MediLink-Frontend/src/app/entities/appointment-interface.ts
+++
@@ -1,7 +0,0 @@
-export interface Appointment {
-  id: number;
-  doctor_id: number;
-  patient_name: string;
-  appointment_date: string;
-  status: string;
-}
--- MediLink-Frontend/src/app/entities/doctor-interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Doctor {
-  id: number;
-  name: string;
-  specialty: string;
-  experience: number;
-}
